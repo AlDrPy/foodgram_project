6b96c1e9cfd2e690fd499445f78f9d8f6576b4ca
Александр Дробязко <al.drobias@yandex.ru>
2023-10-01 11:30:20 +0300

    all models added to admin zone

--- backend/receipts/admin.py
+++ backend/receipts/admin.py
@@ -1,13 +1,55 @@
 from django.contrib import admin
 
+from backend.settings import EMPTY_VALUE
 from receipts.models import (Tag, Ingredient,
                              Receipt, IngredientInReceipt,
                              Favorite, Cart)
 
 
-admin.site.register(Tag)
-admin.site.register(Ingredient)
-admin.site.register(Receipt)
-admin.site.register(IngredientInReceipt)
-admin.site.register(Favorite)
-admin.site.register(Cart)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'color', 'slug')
+    search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug')
+    empty_value_display = EMPTY_VALUE
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = EMPTY_VALUE
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'author', 'total_in_favorites')
+    search_fields = ('name', 'author')
+    list_filter = ('name', 'author', 'tags')
+    empty_value_display = EMPTY_VALUE
+
+    def total_in_favorites(self, obj):
+        return obj.favorites.count()
+
+
+class IngredientInReceiptAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'receipt', 'ingredient', 'amount')
+    empty_value_display = EMPTY_VALUE
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'receipt')
+    search_fields = ('user', 'receipt')
+    empty_value_display = EMPTY_VALUE
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'receipt')
+    search_fields = ('user', 'receipt')
+    empty_value_display = EMPTY_VALUE
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Receipt, ReceiptAdmin)
+admin.site.register(IngredientInReceipt, IngredientInReceiptAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Cart, CartAdmin)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,9 +3,22 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 from users.models import Subscription
 
+from backend.settings import EMPTY_VALUE
 
-User = get_user_model()  # Кастомная модель подтянется сюда, регистрируем
+User = get_user_model()
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'username', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+    empty_value_display = EMPTY_VALUE
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'author')
+    list_filter = ('user', 'author')
+    empty_value_display = EMPTY_VALUE
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Subscription)
+admin.site.register(Subscription, SubscriptionAdmin)
